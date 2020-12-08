98122de44980e79001666111600ecebb309a1918
davidjosephind <davidjosephind@gmail.com>
2020-12-08 16:54:17 +0530

    Complete challenge 16

--- app.js
+++ app.js
@@ -48,6 +48,11 @@ app.post("/compose", function (req, res) {
   res.redirect("/");
 });
 
+app.get("/posts/:postName", function (req, res) {
+  console.log(req.params.postName);
+  res.redirect("/");
+});
+
 app.listen(3000, function () {
   console.log("Server started on port 3000");
 });
